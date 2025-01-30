a321086be9a73d159d444fddb3ea0f40da4ac812
sonika-tang <sonikatang630@gmail.com>
2025-01-30 13:10:46 +0700

    Update

--- index.js
+++ index.js
@@ -1,37 +1,31 @@
-// // Example of JS code, printing on console 
-// const courseName = "Web Developement"; 
-// console.log("Welcome to " + courseName + "!"); 
+// Example of JS code, printing on console 
+const courseName = "Web Developement"; 
+console.log("Welcome to " + courseName + "!"); 
 
-// function calculateSum(array) {
-//     let sum = 0;
-
-//     for (let i = 0; i < array.length; i++) {
-//        // Add here the calculation logic
-//        sum += (i+1);
-//     }
-//     return sum;
-// }
-
-// let arr = [1, 2, 3, 4, 5];
-// console.log(calculateSum(arr));
-
-// function myFunction(min, max) {
-//     var result = "";
-//     for (let number = min; number <= max; number++) {
-//       if (number % 2 === 0) {
-//         result += number + " - ";
-//       }
-//     }
-//     return result;
-//   }
-  
-function myFunction(min, max) {
-    var result = "";
-    for (let number = min; number <= max; number++) {
-      if (number % 2 === 0) {
-        result += number + " - ";
-      }
+//Calculate Sum
+function calculateSum(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+       // Add here the calculation logic
+       sum += (i+1);
     }
-    return result;
-  }
-  console.log(myFunction(7, 3))  
+    return sum;
+}
+let arr = [1, 2, 3, 4, 5];
+console.log(calculateSum(arr));
+  
+//Challenge 1
+function challenge1(width, height) {
+    let rectangleString = '';
+    // Your code
+    for(let i=0; i<=height; i++){
+        for(let j=0; j<=width; j++){
+            rectangleString += "*";
+        }
+        rectangleString += "\n";
+    }
+    return rectangleString;
+}
+console.log(challenge1(3, 4))  
+
+//Challenge 2
